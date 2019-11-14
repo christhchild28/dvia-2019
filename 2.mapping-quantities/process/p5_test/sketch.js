a53104313771a6de0501e55455fc5d719b7403d5
christhchild28 <christiantheodore21@gmail.com>
2019-11-13 19:32:16 -0500

    11.12

--- 2.mapping-quantities/process/p5_test/sketch.js
+++ 2.mapping-quantities/process/p5_test/sketch.js
@@ -1,63 +1,139 @@
 //UseThis-- replace the divergent palette sketch.js with this
 
-var allTests
-var allYield
+//Step 1: Declare variables
+var finalSet; 
 
-//var table
+//Step 2: create array of nuclear tests that draw with data from the csv
+let testArray = [];
 
+
+// Step 3:
 function preload(){
-  allTests = loadTable('data/johnson_all_tests.csv', 'csv', 'header')
-  allYield = loadTable('data/johnson_all_yield.csv', 'csv', 'header')
-  //table = loadTable('data/totals.csv', 'csv', 'header')
+  finalSet = loadTable('data/finalSet.csv', 'csv', 'header')
 }
 
 function setup(){
-  createCanvas(3900, 2250)
+  createCanvas(3200, 700)
   background(127)
   noStroke()
+  fill(255)
   textAlign(CENTER)
   textSize(16)
-
-  // calculate the total number of tests per year (and also the max in any given year)
-  var years = []
-  var totals = []
-  var lowest = 0
-  var highest = 0
-  for (var r=0; r<table.getRowCount(); r++){
-    var sum = 0
-    var year = table.getString(r, 0)
-    for (var c=1; c<table.getColumnCount(); c++){
-      sum += table.getNum(r, c)
-    }
-
-    years.push(year)
-    totals.push(sum)
-    highest = Math.max(sum, highest)
+  for(let i=1; i < finalSet.getRowCount(); i++) {
+    // add this test variable to the array you declared on line 
+    let test = new NuclearTest(
+      // each of these is calling an instance of the class template.
+      // for each row we're creating a new test.
+      finalSet.getColumn('Year')[i],
+      finalSet.getColumn('Country') [i],
+      finalSet.getColumn('Yield')[i],
+      finalSet.getColumn('Location')[i]
+      
+    );
+// the push method: fills the array with the data we stored in the test class
+    testArray.push(test);
   }
 
-  console.log(years)
-  console.log(sum)
-
-  // draw a box for each year and set its color based on the total number of tests
-  var x = 100
-  var y = 100
-  var dim = 50
-  var numberOfShades = 9
-  var palette = Brewer.sequential('BuPu', numberOfShades, lowest, highest)
-
-  for (var i=0; i<years.length; i++){
-    // draw the box
-    var color = palette.colorForValue(totals[i])
-    fill(color)
-    rect(x, y, dim, dim)
-
-    // draw the year number on top
-    fill('white')
-    text(years[i], x+dim*.5, y+dim*.6)
-    x+=dim
+  for(let  i=0; i < testArray.length;  i++) {
+    console.log(testArray[i].kilotons);
+    // DEFINE AN X LOCATION and organize by year -- this will function as our x axis
+    let x = map(testArray[i].year, 1945,1996, 1, 51);
+    // define ellipse sizes by kilotons
+    let kilotons = testArray[i].kilotons;
+    ellipse(x*40, height/2, kilotons, kilotons)
   }
+}
+
+
+
+function draw(){
+
 
 }
+
+//step 1: created a class as a template or each individual test from the CSV
+class NuclearTest {
+  constructor(year, country, kilotons, type) {
+    // we created a constructor function that holds each column, we have a class called tests that contains each of these columns as variables
+    // the class is a template for each column
+    // the country in the test class becomes the country in the constructor
+    this.year = year;
+    this.country = country;
+    this.kilotons = kilotons;
+    this.type = type;
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+// var allTests
+// var allYield
+
+// //var table
+
+// function preload(){
+//   allTests = loadTable('data/johnson_all_tests.csv', 'csv', 'header')
+//   allYield = loadTable('data/johnson_all_yield.csv', 'csv', 'header')
+//   console.log(allTests);
+
+//   //table = loadTable('data/totals.csv', 'csv', 'header')
+// }
+
+// function setup(){
+//   createCanvas(3900, 2250)
+//   background(127)
+//   noStroke()
+//   textAlign(CENTER)
+//   textSize(16)
+
+//   // calculate the total number of tests per year (and also the max in any given year)
+//   var years = []
+//   var totals = []
+//   var lowest = 0
+//   var highest = 0
+//   for (var r=0; r<table.getRowCount(); r++){
+//     var sum = 0
+//     var year = table.getString(r, 0)
+//     for (var c=1; c<table.getColumnCount(); c++){
+//       sum += table.getNum(r, c)
+//     }
+
+//     years.push(year)
+//     totals.push(sum)
+//     highest = Math.max(sum, highest)
+//   }
+
+//   console.log(years)
+//   console.log(sum)
+
+//   // draw a box for each year and set its color based on the total number of tests
+//   var x = 100
+//   var y = 100
+//   var dim = 50
+//   var numberOfShades = 9
+//   var palette = Brewer.sequential('BuPu', numberOfShades, lowest, highest)
+
+//   for (var i=0; i<years.length; i++){
+//     // draw the box
+//     var color = palette.colorForValue(totals[i])
+//     fill(color)
+//     rect(x, y, dim, dim)
+
+//     // draw the year number on top
+//     fill('white')
+//     text(years[i], x+dim*.5, y+dim*.6)
+//     x+=dim
+//   }
+
+// }
 
 
 
